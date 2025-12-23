3aa2b2d3636ade570a5c8e127a66b20551563ed3
FelipeAidar <felipeaidar.split@gmail.com>
2025-12-23 02:42:34 -0300

    Removed Reference

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Sirenix.OdinInspector;
+//using Sirenix.OdinInspector;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
